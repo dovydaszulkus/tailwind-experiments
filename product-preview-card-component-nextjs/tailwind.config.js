3e51867b6dc1ff7be54f5f729d1a2b471040c8bf
dovydaszulkus <dovydaszulkus@gmail.com>
2022-09-04 21:41:50 +0100

    Add image assets and add brand colors to tailwind config

--- product-preview-card-component-nextjs/tailwind.config.js
+++ product-preview-card-component-nextjs/tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-cyan': 'hsl(158, 36%, 37%)',
+        'cream': 'hsl(30, 38%, 92%)',
+        'very-dark-blue': 'hsl(212, 21%, 14%)',
+        'dark-grayish-blue': 'hsl(228, 12%, 48%)',
+        'white': 'hsl(0, 0%, 100%)'
+      },
+    },
   },
   plugins: [],
 }
